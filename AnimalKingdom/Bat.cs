80fec5ac89a1ffd07a85b096e7469b2ebd63b305
Daniela Sousa <danielasousa.info@gmail.com>
2024-04-25 22:49:48 +0100

    int NumberOfWings and NumberOfNipples

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -2,6 +2,8 @@ namespace AnimalKingdom
 {
     public class Bat : Animal
     {
+        public int NumberOfWings { get; } = 2;
+        public int NumberOfNipples { get; } = 2;
         public override string Sound()
         {
             return base.Sound() + "ti ti ti ti ti";
