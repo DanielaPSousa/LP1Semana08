b3aa36d80ddc903874958590e4d761007af16275
Daniela Sousa <danielasousa.info@gmail.com>
2024-04-25 22:50:49 +0100

    interface para o NumberOfNipples

--- AnimalKingdom/IMammal.cs
+++ AnimalKingdom/IMammal.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace 
+namespace AnimalKingdom
 {
-    public class IMammal
+    public interface IMammal
     {
-        
+        int NumberOfNipples { get; }
     }
 }
