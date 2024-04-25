d85e2e552ca1a82bb9609d9e10cd6f305c2f3cec
Daniela Sousa <danielasousa.info@gmail.com>
2024-04-25 22:49:07 +0100

    int NumberOfWings

--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -2,6 +2,7 @@ namespace AnimalKingdom
 {
     public class Bee : Animal
     {
+        public int NumberOfWings { get; } = 4;
         public override string Sound()
         {
             return base.Sound() + "Bzz";
